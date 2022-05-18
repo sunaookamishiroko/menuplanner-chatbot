1044924b72f192a5e9a8ac716e0eddf06d5462f6
이서윤 <idfn3535@gmail.com>
2022-05-19 02:01:39 +0900

    Feat : 자바 스프링 프로젝트 추가

---
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/MenuplannerChatbotApiApplication.java
@@ -0,0 +1,13 @@
+package madeby.seoyun.menuplannerchatbotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuplannerChatbotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuplannerChatbotApiApplication.class, args);
+	}
+
+}
---
+++ menuplanner-chatbot-api/src/test/java/madeby/seoyun/menuplannerchatbotapi/MenuplannerChatbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package madeby.seoyun.menuplannerchatbotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuplannerChatbotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
