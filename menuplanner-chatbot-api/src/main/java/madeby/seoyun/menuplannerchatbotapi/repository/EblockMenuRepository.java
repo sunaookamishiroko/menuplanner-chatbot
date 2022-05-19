5f91f6de6a25761c27d53f19f3588a63cb3d2a97
이서윤 <idfn3535@gmail.com>
2022-05-19 21:14:41 +0900

    Feat : japrepository 추가

--- menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/repository/EblockMenuRepository.java
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/repository/EblockMenuRepository.java
@@ -1,7 +1,9 @@
 package madeby.seoyun.menuplannerchatbotapi.repository;
 
+import madeby.seoyun.menuplannerchatbotapi.model.EblockMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class EblockMenuRepository {
+public interface EblockMenuRepository extends JpaRepository<EblockMenu, String> {
 }
