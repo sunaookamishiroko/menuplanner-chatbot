cb3295a414392fa151fbba76279ff870acd38f95
이서윤 <idfn3535@gmail.com>
2022-12-14 19:34:15 +0900

    Feat : 서버 운영 정보 설정, 체크 메서드 추가 및 수정

--- menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/component/SettingProperty.java
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/component/SettingProperty.java
@@ -1,14 +1,15 @@
 package madeby.seoyun.menuplannerchatbotapi.component;
 
 import madeby.seoyun.menuplannerchatbotapi.model.RestaurantProperty;
-import madeby.seoyun.menuplannerchatbotapi.repository.RestaurantInfoRepository;
+import madeby.seoyun.menuplannerchatbotapi.model.ServerInfo;
+import madeby.seoyun.menuplannerchatbotapi.repository.ServerInfoRepository;
 import madeby.seoyun.menuplannerchatbotapi.repository.RestaurantPropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 식당 정보, 방학 여부 등을 체크하여 세팅하기 위한 컴포넌트
+ * 식당 정보, 서버 운영 정보를 체크하여 세팅하기 위한 컴포넌트
  *
  * @filename : SettingProperty.java
  * @Author : lsy
@@ -17,14 +18,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class SettingProperty {
 
     private final RestaurantPropertyRepository restaurantPropertyRepository;
-    private final RestaurantInfoRepository restaurantInfoRepository;
-
-    private boolean isVacation;
+    private final ServerInfoRepository serverInfoRepository;
 
     @Autowired
-    public SettingProperty(RestaurantPropertyRepository restaurantPropertyRepository, RestaurantInfoRepository restaurantInfoRepository) {
+    public SettingProperty(RestaurantPropertyRepository restaurantPropertyRepository,
+                           ServerInfoRepository serverInfoRepository) {
         this.restaurantPropertyRepository = restaurantPropertyRepository;
-        this.restaurantInfoRepository = restaurantInfoRepository;
+        this.serverInfoRepository = serverInfoRepository;
     }
 
 
@@ -49,20 +49,21 @@ public class SettingProperty {
     }
 
     /**
-     * 식당 정보가 존재하는지 체크한다.
+     * 서버 운영에 필요한 정보가 존재하는지 체크한다.
      *
      * @ param : 없음
      * @ return boolean : 존재하면 true, 존재하지 않으면 false
      */
     @Transactional(readOnly = true)
-    public boolean isVacationExist() {
-        LogData.printLog("방학 모드 확인...", "isVacationExist");
+    public boolean isServerInfoExist() {
+        LogData.printLog("서버 운영 정보가 존재하는지 확인...", "isServerInfoExist");
 
-        if(restaurantInfoRepository.findById(0).isPresent()) {
-            LogData.printLog("방학 모드 데이터가 존재합니다...", "isVacationExist");
+        if(serverInfoRepository.findById(0).isPresent()&&
+                serverInfoRepository.findById(1).isPresent()) {
+            LogData.printLog("서버 운영 정보가 존재합니다.", "isServerInfoExist");
             return true;
         } else {
-            LogData.printLog("방학 모드 데이터가 존재하지 않습니다...", "isVacationExist");
+            LogData.printLog("서버 운영 정보가 존재하지 않습니다.", "isServerInfoExist");
             return false;
         }
     }
@@ -99,28 +100,33 @@ public class SettingProperty {
     }
 
     /**
-     * 방학 모드를 설정한다.
+     * 임의의 서버 운영 정보를 저장한다.
      *
      * @ param : 없음
      * @ return : 없음
      */
-    public void setVacation() {
-        if(restaurantInfoRepository.findById(0).get().isVacation()) {
-            LogData.printLog("방학 모드 = true", "setVacation");
-            isVacation = true;
-        } else {
-            LogData.printLog("방학 모드 = false", "setVacation");
-            isVacation = false;
-        }
+    @Transactional(rollbackFor = Exception.class)
+    public void saveServerInfo() {
+        LogData.printLog("서버 운영 정보 저장중...", "saveServerInfo");
+
+        serverInfoRepository.save(new ServerInfo(0, false));
+        serverInfoRepository.save(new ServerInfo(1, false));
+
+        LogData.printLog("저장 완료", "saveServerInfo");
     }
 
     /**
-     * 방학 모드인지 아닌지 알려준다.
+     * 인자로 받은 식당의 방학 모드 여부를 return한다.
      *
-     * @ param : 없음
-     * @ return : boolean : 방학이면 true, 학기중이면 false
+     * @ param int id : 식당 식별 번호
+     * @ return boolean : 방학이면 true, 학기중이면 false
      */
-    public boolean checkVacation() {
-        return isVacation;
+    @Transactional(readOnly = true)
+    public boolean checkIsVacation(int id) {
+        if(serverInfoRepository.findById(id).orElseThrow().isVacation()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
