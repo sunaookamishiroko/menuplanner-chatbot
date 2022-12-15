19651e495c5ff5c880249419349d810a74178bf5
이서윤 <idfn3535@gmail.com>
2022-12-16 01:10:03 +0900

    Refacfor : 키 생성 전략 변경

--- menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/model/RestaurantMenu.java
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/model/RestaurantMenu.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Table(name = "restaurantMenu")
 public class RestaurantMenu {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String date;
