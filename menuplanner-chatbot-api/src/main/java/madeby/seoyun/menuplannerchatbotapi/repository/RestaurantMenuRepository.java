9836e6f4d9cad4ed9e64c24560999a3ececee4b0
이서윤 <idfn3535@gmail.com>
2022-12-15 23:24:20 +0900

    Fix : transactinal 추가

--- menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/repository/RestaurantMenuRepository.java
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/repository/RestaurantMenuRepository.java
@@ -4,6 +4,7 @@ import madeby.seoyun.menuplannerchatbotapi.model.RestaurantMenu;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 식당 메뉴 쿼리를 위한 JpaRespository를 구현한 인터페이스
@@ -15,5 +16,7 @@ import org.springframework.stereotype.Repository;
 public interface RestaurantMenuRepository extends JpaRepository<RestaurantMenu, Long> {
     @EntityGraph(attributePaths = "restaurantProperty")
     RestaurantMenu findByRestaurantPropertyIdAndDate(int restaurantCode, String date);
+
+    @Transactional(rollbackFor = Exception.class)
     void deleteAllByRestaurantPropertyId(int restaurantCode);
 }
