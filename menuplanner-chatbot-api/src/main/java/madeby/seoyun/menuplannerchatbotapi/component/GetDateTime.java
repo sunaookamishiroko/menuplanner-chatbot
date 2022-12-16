f848905a29b288a86cc6ea20cf49edb8988d3350
이서윤 <idfn3535@gmail.com>
2022-12-16 22:02:38 +0900

    Feat : Clock을 주입 받아 사용하도록 변경

--- menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/component/GetDateTime.java
+++ menuplanner-chatbot-api/src/main/java/madeby/seoyun/menuplannerchatbotapi/component/GetDateTime.java
@@ -1,7 +1,9 @@
 package madeby.seoyun.menuplannerchatbotapi.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Clock;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.format.TextStyle;
@@ -15,7 +17,13 @@ import java.util.Locale;
  */
 @Component
 public class GetDateTime {
-    public static LocalDateTime localDateTime;
+    private LocalDateTime localDateTime;
+    private final Clock clock;
+
+    @Autowired
+    public GetDateTime(Clock clock) {
+        this.clock = clock;
+    }
 
     /**
      * 오늘의 날짜 (월, 일)을 "x월 x일"의 형태로 반환한다.
@@ -23,8 +31,8 @@ public class GetDateTime {
      * @ param : 없음
      * @ return String : "x월 x일" 형태의 문자열
      */
-    public static String getDate() {
-        localDateTime = LocalDateTime.now();
+    public String getDate() {
+        localDateTime = LocalDateTime.now(clock);
         return localDateTime.getMonth().getValue() + "월 " + localDateTime.getDayOfMonth() + "일";
     }
 
@@ -34,8 +42,8 @@ public class GetDateTime {
      * @ param : 없음
      * @ return String : "월", "화" 등의 형태의 문자열
      */
-    public static String getDayOfWeek() {
-        localDateTime = LocalDateTime.now();
+    public String getDayOfWeek() {
+        localDateTime = LocalDateTime.now(clock);
         DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
         return dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN);
     }
